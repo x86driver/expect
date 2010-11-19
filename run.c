6ab9b88e40d7418e3cfb4317f45ea2ad03f8787e
doremi <nothing>
2010-11-19 08:54:02 +0800

    可以讓使用者指定時間 然後到達時間後執行某件事

--- run.c
+++ run.c
@@ -8,15 +8,33 @@ extern char **environ;
 
 void sig(int signo)
 {
+        int ret = execve("./upload.exp", NULL, environ);
+        if (ret == -1)
+                perror("execve");
+
 	printf("Aha!\n");
 	exit(1);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	time_t t = time(NULL);
-	struct tm *tm = localtime(&t);
-	printf("%d:%d\n", tm->
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        printf("Now: %d:%d:%d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+	if (argc != 3) {
+		printf("Usage: %s [hour] [min]\n", argv[0]);
+		exit(1);
+	}
+
+	int f_hour = atoi(argv[1]);
+	int f_min = atoi(argv[2]);
+	int offset_h = f_hour - tm->tm_hour;
+	int offset_m = f_min - tm->tm_min;
+	int offset_sec = offset_h * 60 * 60 + offset_m * 60;
+
+	printf("Run: %d:%d\n", f_hour, f_min);
+	printf("Offset: %d seconds\n", offset_sec);
 
 	struct sigaction act;
 	act.sa_handler = sig;
@@ -24,11 +42,10 @@ int main()
 	act.sa_flags = 0;
 	if (sigaction(SIGALRM, &act, NULL) < 0)
 		perror("sigaction");
-//	int ret = execve("./upload.exp", NULL, environ);
-//	if (ret == -1)
-//		perror("execve");
-	alarm(1);
+
+	alarm(offset_sec);
 	pause();
+
 	printf("no~\n");
 	return 0;
 }
